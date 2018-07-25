fa6a7c4a263d480cfffe8a43d08d4b784adb37b4
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2018-07-25 03:52:22 -0500

    add more test cases to App component

--- src/Components/App/index.spec.js
+++ src/Components/App/index.spec.js
@@ -1,5 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import App from 'Components/App'
+import { Provider } from 'Store'
+
 describe('<App />', () => {
-  it('Should true be true', () => {
-    expect(true).toBeTruthy()
+  it('Should be rendered succesfully', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper).toHaveLength(1)
+  })
+
+  it('Should children be passed as prop', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.props()).toHaveProperty('children')
+  })
+
+  it('Should Provider be preset as child', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.find(Provider)).toHaveLength(1)
   })
 })
