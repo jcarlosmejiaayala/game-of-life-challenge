8f7ecd94d52b1cb3fc36329462fddd8646d4e0f3
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2018-07-27 08:23:40 -0500

    fix unit tests

--- src/Components/Cell/unit.js
+++ src/Components/Cell/unit.js
@@ -3,15 +3,22 @@ import { shallow } from 'enzyme'
 
 import Cell from 'Components/Cell'
 
+const props = {
+  isAlive: true,
+  switchLiveness: jest.fn(),
+  x: 0,
+  y: 0
+}
+
 describe('<Cell />', () => {
   it('Should be rendered succesfully', () => {
-    const wrapper = shallow(<Cell />)
+    const wrapper = shallow(<Cell {...props} />)
 
     expect(wrapper).toHaveLength(1)
   })
 
   it('Should IsAlive be passed as props and false as value', () => {
-    const wrapper = shallow(<Cell isAlive />)
+    const wrapper = shallow(<Cell {...props} />)
 
     expect(wrapper.props().isAlive).toBeDefined()
     expect(wrapper.props().isAlive).toBeTruthy()
