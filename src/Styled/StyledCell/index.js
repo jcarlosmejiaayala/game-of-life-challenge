6985258c0fd6285d4064b13c2aee92f8702fcacf
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2018-07-27 08:52:52 -0500

    add visual hint to click on cells

--- src/Styled/StyledCell/index.js
+++ src/Styled/StyledCell/index.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
 const StyledCell = styled.button`
+  animation: 0.5s all linear;
+  background-color: ${({ isAlive }) => (isAlive ? 'green' : 'white')};
   border: 1px solid black;
   height: 20px;
   width: 20px;
-  background-color: ${({ isAlive }) => (isAlive ? 'green' : 'white')};
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.2);
+  }
 `
 
 export default StyledCell
